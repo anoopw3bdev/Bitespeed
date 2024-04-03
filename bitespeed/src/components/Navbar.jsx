f72933daf0fdfcc82917d81c1d914f2f8c34ece4
anoop <anoopano20@gmail.com>
2024-04-04 00:13:24 +0530

    feat: implment error message section

--- bitespeed/src/components/Navbar.jsx
+++ bitespeed/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import "../assets/styles/Navbar.css";
 
-export const Navbar = () => {
+export const Navbar = ({ hasError, validateOnSave }) => {
   return (
     <div className="navbar">
-      <button>Save Changes</button>
+      {hasError && (
+        <span className="navbar__error-section">
+          Cannot save flow
+        </span>
+      )}
+      <button onClick={validateOnSave}>Save Changes</button>
     </div>
   );
 };
